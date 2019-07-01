49c76442bbe92f047e114d9a743590104e491080
takashi_onawa <onawatakashi@gmail.com>
2019-07-02 00:04:37 +0900

    アプリケーション層のフォルダ構成を変更。（コミット漏れ）

--- src/EquipmentReservation.Application/Services/Commands/RegisterReservationCommand.cs
+++ src/EquipmentReservation.Application/Services/Commands/RegisterReservationCommand.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace EquipmentReservation.Application.Commands
+namespace EquipmentReservation.Application.Services.Commands
 {
     public class RegisterReservationCommand
     {
